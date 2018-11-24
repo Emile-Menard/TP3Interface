b07e4c2e75ef638dc9758d660beb97a4bd324a10
Dominic200 <45239250+Dominic200@users.noreply.github.com>
2018-11-24 12:01:57 -0500

    Liste noire pas fini
    Liste noire pas fini

--- Client_PM_03/PageListeNoire.cs
+++ Client_PM_03/PageListeNoire.cs
@@ -7,14 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PhotoManagerClient;
+using Validation;
 
 namespace Client_PM
 {
+    // Pas fini en construction
     public partial class PageListeNoire : Form
     {
+        public User lUser;
+        public Photo lPhoto;
+        public List <User> ListeUsers;
+        
         public PageListeNoire()
         {
             InitializeComponent();
+            FillCombobox();
+        }
+
+        private void FillCombobox()
+        {
+            foreach (var user in ListeUsers)
+            {
+                if (!LISTB_Liste_Noire.Items.Contains(user.Name))
+                {
+                    COB_Liste_Noire_Utilisateur.Items.Add(user.Name);
+                }
+            }
+        }
+
+        private void FBTN_Add_User_Black_List_Click(object sender, EventArgs e)
+        {
+            LISTB_Liste_Noire.Items.Add(COB_Liste_Noire_Utilisateur.SelectedItem);
+            UpdateComboAndListe();
+        }
+
+        private void UpdateComboAndListe()
+        {
+            FillCombobox();
+            COB_Liste_Noire_Utilisateur.Refresh();
+            LISTB_Liste_Noire.Refresh();
+        }
+
+        private void FBTN_Delete_Liste_User_Click(object sender, EventArgs e)
+        {
+            LISTB_Liste_Noire.Items.Remove(LISTB_Liste_Noire.SelectedItem);
+            UpdateComboAndListe();
+        }
+
+        private void F_BTN_Clean_Black_List_Click(object sender, EventArgs e)
+        {
+            LISTB_Liste_Noire.Items.Clear();
+            UpdateComboAndListe();
         }
     }
 }
--- Client_PM_03/PageListeNoire.designer.cs
+++ Client_PM_03/PageListeNoire.designer.cs
@@ -28,41 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PageListeNoire));
+            this.COB_Liste_Noire_Utilisateur = new System.Windows.Forms.ComboBox();
+            this.LISTB_Liste_Noire = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.FBTN_Add_User_Black_List = new PhotoManagerClient.FlashButton();
+            this.FBTN_Delete_Liste_User = new PhotoManagerClient.FlashButton();
+            this.F_BTN_Clean_Black_List = new PhotoManagerClient.FlashButton();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // COB_Liste_Noire_Utilisateur
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(32, 50);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(167, 21);
-            this.comboBox1.TabIndex = 0;
+            this.COB_Liste_Noire_Utilisateur.FormattingEnabled = true;
+            this.COB_Liste_Noire_Utilisateur.Location = new System.Drawing.Point(32, 50);
+            this.COB_Liste_Noire_Utilisateur.Name = "COB_Liste_Noire_Utilisateur";
+            this.COB_Liste_Noire_Utilisateur.Size = new System.Drawing.Size(167, 21);
+            this.COB_Liste_Noire_Utilisateur.TabIndex = 0;
             // 
-            // button1
+            // LISTB_Liste_Noire
             // 
-            this.button1.Location = new System.Drawing.Point(220, 50);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Ajouter";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(32, 105);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(263, 160);
-            this.listBox1.TabIndex = 2;
+            this.LISTB_Liste_Noire.FormattingEnabled = true;
+            this.LISTB_Liste_Noire.Location = new System.Drawing.Point(32, 105);
+            this.LISTB_Liste_Noire.Name = "LISTB_Liste_Noire";
+            this.LISTB_Liste_Noire.Size = new System.Drawing.Size(263, 160);
+            this.LISTB_Liste_Noire.TabIndex = 2;
             // 
             // button2
             // 
@@ -118,21 +112,73 @@
             this.button5.Text = "Reinitialiser";
             this.button5.UseVisualStyleBackColor = true;
             // 
-            // Form4
+            // FBTN_Add_User_Black_List
+            // 
+            this.FBTN_Add_User_Black_List.BackgroundImage = global::Client_PM.Properties.Resources.Plus;
+            this.FBTN_Add_User_Black_List.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.FBTN_Add_User_Black_List.ClickedImage = null;
+            this.FBTN_Add_User_Black_List.DisabledImage = null;
+            this.FBTN_Add_User_Black_List.Image = ((System.Drawing.Image)(resources.GetObject("FBTN_Add_User_Black_List.Image")));
+            this.FBTN_Add_User_Black_List.Location = new System.Drawing.Point(237, 43);
+            this.FBTN_Add_User_Black_List.Name = "FBTN_Add_User_Black_List";
+            this.FBTN_Add_User_Black_List.NeutralImage = null;
+            this.FBTN_Add_User_Black_List.OverImage = null;
+            this.FBTN_Add_User_Black_List.Size = new System.Drawing.Size(32, 32);
+            this.FBTN_Add_User_Black_List.TabIndex = 9;
+            this.FBTN_Add_User_Black_List.UseVisualStyleBackColor = true;
+            this.FBTN_Add_User_Black_List.Click += new System.EventHandler(this.FBTN_Add_User_Black_List_Click);
+            // 
+            // FBTN_Delete_Liste_User
+            // 
+            this.FBTN_Delete_Liste_User.BackgroundImage = global::Client_PM.Properties.Resources.Delete;
+            this.FBTN_Delete_Liste_User.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.FBTN_Delete_Liste_User.ClickedImage = null;
+            this.FBTN_Delete_Liste_User.DisabledImage = null;
+            this.FBTN_Delete_Liste_User.Image = ((System.Drawing.Image)(resources.GetObject("FBTN_Delete_Liste_User.Image")));
+            this.FBTN_Delete_Liste_User.Location = new System.Drawing.Point(275, 41);
+            this.FBTN_Delete_Liste_User.Name = "FBTN_Delete_Liste_User";
+            this.FBTN_Delete_Liste_User.NeutralImage = null;
+            this.FBTN_Delete_Liste_User.OverImage = null;
+            this.FBTN_Delete_Liste_User.Size = new System.Drawing.Size(36, 36);
+            this.FBTN_Delete_Liste_User.TabIndex = 10;
+            this.FBTN_Delete_Liste_User.Text = "flashButton1";
+            this.FBTN_Delete_Liste_User.UseVisualStyleBackColor = true;
+            this.FBTN_Delete_Liste_User.Click += new System.EventHandler(this.FBTN_Delete_Liste_User_Click);
+            // 
+            // F_BTN_Clean_Black_List
+            // 
+            this.F_BTN_Clean_Black_List.BackgroundImage = global::Client_PM.Properties.Resources.ArrowRotation;
+            this.F_BTN_Clean_Black_List.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.F_BTN_Clean_Black_List.ClickedImage = null;
+            this.F_BTN_Clean_Black_List.DisabledImage = null;
+            this.F_BTN_Clean_Black_List.Image = ((System.Drawing.Image)(resources.GetObject("F_BTN_Clean_Black_List.Image")));
+            this.F_BTN_Clean_Black_List.Location = new System.Drawing.Point(46, 301);
+            this.F_BTN_Clean_Black_List.Name = "F_BTN_Clean_Black_List";
+            this.F_BTN_Clean_Black_List.NeutralImage = null;
+            this.F_BTN_Clean_Black_List.OverImage = null;
+            this.F_BTN_Clean_Black_List.Size = new System.Drawing.Size(36, 36);
+            this.F_BTN_Clean_Black_List.TabIndex = 11;
+            this.F_BTN_Clean_Black_List.Text = "flashButton1";
+            this.F_BTN_Clean_Black_List.UseVisualStyleBackColor = true;
+            this.F_BTN_Clean_Black_List.Click += new System.EventHandler(this.F_BTN_Clean_Black_List_Click);
+            // 
+            // PageListeNoire
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(329, 364);
+            this.Controls.Add(this.F_BTN_Clean_Black_List);
+            this.Controls.Add(this.FBTN_Delete_Liste_User);
+            this.Controls.Add(this.FBTN_Add_User_Black_List);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
-            this.Name = "Form4";
+            this.Controls.Add(this.LISTB_Liste_Noire);
+            this.Controls.Add(this.COB_Liste_Noire_Utilisateur);
+            this.Name = "PageListeNoire";
             this.Text = "Liste noire";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -141,14 +187,16 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox COB_Liste_Noire_Utilisateur;
+        private System.Windows.Forms.ListBox LISTB_Liste_Noire;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private PhotoManagerClient.FlashButton FBTN_Add_User_Black_List;
+        private PhotoManagerClient.FlashButton FBTN_Delete_Liste_User;
+        private PhotoManagerClient.FlashButton F_BTN_Clean_Black_List;
     }
 }
