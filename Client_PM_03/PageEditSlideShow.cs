b5742928f8f3f818a3121f639af69db5a78af490
Émile Ménard <emile.menard@outlook.com>
2018-12-04 11:49:51 -0500

    Arrangé le titre et comments

--- Client_PM_03/PageEditSlideShow.cs
+++ Client_PM_03/PageEditSlideShow.cs
@@ -13,10 +13,7 @@ namespace Client_PM
 {
     public partial class PageEditSlideShow : Form
     {
-        // Liste des id des photos incluses dans le diaporama
         public List<int> SlideShowList { get; set; }
-
-        // Liste de toutes les photos éligibles au diaporama
         public List<Photo> PhotoPool { get; set; }
 
         public PageEditSlideShow()
@@ -27,15 +24,13 @@ namespace Client_PM
 
         private void DLG_ManageSildeShow_Shown(object sender, EventArgs e)
         {
-            WaitSplash.Show(this, "Loading photos...");
+            WaitSplash.Show(this, "Chargement des photos...");
             Load_Photos();
             WaitSplash.Hide();
         }
 
-        // Ajouter les images des photos dans le layout
         private void Load_Photos()
         {
-            // Suspendre la mise à jour 
             SuspendLayout();
             foreach (Photo photo in PhotoPool)
             {
@@ -43,18 +38,14 @@ namespace Client_PM
                 if (pp != null)
                     FLPN_Photos.Controls.Add(pp);
             }
-            // Reprendre la mise à jour
             ResumeLayout();
         }
 
-        // Construire un panneaux de photo sélectionnable
         private PhotoPanel MakePhotoPanel(int photoId)
         {
-            //Photo photo = DBPhotosWebServices.GetPhoto(photoId);
             Photo photo = PhotoPool.Where(p => p.Id == photoId).First<Photo>();
             if (photo != null)
             {
-                // Boîte d'image
                 PictureBox pbx = new PictureBox();
                 pbx.Size = new Size(80, 80);
                 pbx.BackgroundImageLayout = ImageLayout.Zoom;
@@ -62,11 +53,8 @@ namespace Client_PM
                 pbx.Location = new Point(10, 10);
                 pbx.Click += PBX_Click;
 
-                // panneaux de photo sélectionnable
                 PhotoPanel photoPanel = new PhotoPanel();
                 photoPanel.Size = new Size(100, 100);
-                // Définir son nom en incluant le id de la photo 
-                // permettant ainsi de retrouver la photo concernée par un clic
                 photoPanel.Name = "PN_" + photoId.ToString();
                 photoPanel.Click += Photo_Click;
                 photoPanel.Selected = InSlideShow(photoId);
@@ -81,7 +69,6 @@ namespace Client_PM
         {
             PictureBox PBX = (PictureBox)sender;
             PhotoPanel PPN = PBX.Parent as PhotoPanel;
-            // Extraire le id de la photo concernée
             string stringId = PPN.Name.Substring(PPN.Name.IndexOf("_") + 1);
             int photoId = int.Parse(stringId);
             AddRemove_SlideShow(photoId);
@@ -89,12 +76,9 @@ namespace Client_PM
             PPN.Refresh();
         }
 
-        // Gestionnaire est utile dans le cas ou le clic aurait lieu à l'extérieur de la boîte de photo
-        // mais à l'intérieur de son panneau
         private void Photo_Click(object sender, EventArgs e)
         {
             PhotoPanel PPN = (PhotoPanel)sender;
-            // Extraire le id de la photo concernée
             string stringId = PPN.Name.Substring(PPN.Name.IndexOf("_") + 1);
             int photoId = int.Parse(stringId);
             AddRemove_SlideShow(photoId);
@@ -107,7 +91,6 @@ namespace Client_PM
             return SlideShowList.IndexOf(photoId) != -1;
         }
 
-        // Ajouter ou retirer l'index de photo
         private void AddRemove_SlideShow(int photoId)
         {
             int index = SlideShowList.IndexOf(photoId);
@@ -142,10 +125,7 @@ namespace Client_PM
 
             Properties.Settings.Default.LocationPageEditCarousel = this.Location;
 
-            // Save settings
             Properties.Settings.Default.Save();
- 
-
         }
     }
 }
